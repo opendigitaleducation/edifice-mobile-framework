cdefadb76e9256e12c19fbf2e75c60b35bfcbebc
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-12-15 12:22:47 +0100

    chore(dark more): MB-2339, update theme

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,10 +1,15 @@
 /**
- * Theme declaration and overloading system.
+ * Theme declaration and overloading system
  */
 import deepmerge from 'deepmerge';
-import { ColorValue } from 'react-native';
+import { Appearance, ColorValue, useColorScheme } from 'react-native';
+import { connect } from 'react-redux';
 
+import { ThunkDispatch } from 'redux-thunk';
 import customTheme from '~/app/override/theme';
+import { IGlobalState, getStore } from '~/app/store';
+import { actionTypes } from '~/framework/modules/user/reducer';
+import { getItemJson, setItemJson } from '~/framework/util/storage';
 
 //  8888888          888                      .d888
 //    888            888                     d88P"
@@ -25,42 +30,85 @@ export interface IShades {
 
 export interface ITheme {
   // Color palette used globally
-  palette: {
-    primary: IShades;
-    secondary: IShades;
-    complementary: {
-      red: IShades;
-      orange: IShades;
-      yellow: IShades;
-      green: IShades;
-      blue: IShades;
-      indigo: IShades;
-      purple: IShades;
-      pink: IShades;
+  light: {
+    palette: {
+      primary: IShades;
+      secondary: IShades;
+      complementary: {
+        red: IShades;
+        orange: IShades;
+        yellow: IShades;
+        green: IShades;
+        blue: IShades;
+        indigo: IShades;
+        purple: IShades;
+        pink: IShades;
+      };
+      grey: {
+        darkness: ColorValue;
+        black: ColorValue;
+        graphite: ColorValue;
+        stone: ColorValue;
+        grey: ColorValue;
+        cloudy: ColorValue;
+        pearl: ColorValue;
+        fog: ColorValue;
+        white: ColorValue;
+      };
+      status: {
+        info: IShades;
+        success: IShades;
+        failure: IShades;
+        warning: IShades;
+      };
+      flashMessages: {
+        'grey-dark': ColorValue;
+        red: ColorValue;
+        orange: ColorValue;
+        green: ColorValue;
+        blue: ColorValue;
+      };
     };
-    grey: {
-      darkness: ColorValue;
-      black: ColorValue;
-      graphite: ColorValue;
-      stone: ColorValue;
-      grey: ColorValue;
-      cloudy: ColorValue;
-      pearl: ColorValue;
-      fog: ColorValue;
-      white: ColorValue;
-    };
-    status: {
-      info: IShades;
-      success: IShades;
-      failure: IShades;
-      warning: IShades;
-    };
-    flashMessages: {
-      'grey-dark': ColorValue;
-      red: ColorValue;
-      orange: ColorValue;
-      green: ColorValue;
-      blue: ColorValue;
+  };
+
+  dark: {
+    palette: {
+      primary: IShades;
+      secondary: IShades;
+      complementary: {
+        red: IShades;
+        orange: IShades;
+        yellow: IShades;
+        green: IShades;
+        blue: IShades;
+        indigo: IShades;
+        purple: IShades;
+        pink: IShades;
+      };
+      grey: {
+        darkness: ColorValue;
+        black: ColorValue;
+        graphite: ColorValue;
+        stone: ColorValue;
+        grey: ColorValue;
+        cloudy: ColorValue;
+        pearl: ColorValue;
+        fog: ColorValue;
+        white: ColorValue;
+      };
+      status: {
+        info: IShades;
+        success: IShades;
+        failure: IShades;
+        warning: IShades;
+      };
+      flashMessages: {
+        'grey-dark': ColorValue;
+        red: ColorValue;
+        orange: ColorValue;
+        green: ColorValue;
+        blue: ColorValue;
+      };
     };
   };
 
@@ -119,7 +167,7 @@ export interface ITheme {
   };
 }
 
-type ThemeInitializer = Pick<ITheme, 'palette'> & {
+type ThemeInitializer = Pick<ITheme, 'light' | 'dark'> & {
   init(): ITheme;
 };
 
@@ -135,102 +183,205 @@ export const defaultTheme: ThemeInitializer = {
 
   // Magenta color indicated non-defined values
 
-  palette: {
-    primary: {
-      evil: 'magenta',
-      dark: '#1B84AC',
-      regular: '#2A9CC8',
-      light: '#AADAED',
-      pale: '#E4F4FF',
-    },
-    secondary: {
-      evil: 'magenta',
-      dark: '#F17A17',
-      regular: '#FF8D2E',
-      light: '#FFC696',
-      pale: '#FFEFE3',
-    },
-    complementary: {
-      red: {
-        evil: 'magenta',
-        dark: '#C82222',
-        regular: '#E13A3A',
-        light: '#F48A8A',
-        pale: '#FFD9D9',
-      },
-      orange: {
-        evil: 'magenta',
-        dark: '#F17A17',
-        regular: '#FF8D2E',
-        light: '#FFC696',
-        pale: '#FFEFE3',
-      },
-      yellow: {
-        evil: 'magenta',
-        dark: '#DAA910',
-        regular: '#ECBE30',
-        light: '#F6DE94',
-        pale: '#FFF4D1',
-      },
-      green: {
-        evil: 'magenta',
-        dark: '#33A797',
-        regular: '#46BFAF',
-        light: '#A2E0D8',
-        pale: '#E7F5F4',
-      },
-      blue: {
+  light: {
+    palette: {
+      primary: {
         evil: 'magenta',
         dark: '#1B84AC',
         regular: '#2A9CC8',
         light: '#AADAED',
         pale: '#E4F4FF',
       },
-      indigo: {
+      secondary: {
         evil: 'magenta',
-        dark: '#121982',
-        regular: '#1A22A2',
-        light: '#9297E5',
-        pale: '#DDE8FD',
+        dark: '#F17A17',
+        regular: '#FF8D2E',
+        light: '#FFC696',
+        pale: '#FFEFE3',
       },
-      purple: {
-        evil: 'magenta',
-        dark: '#5D1D79',
-        regular: '#763294',
-        light: '#B68ACA',
-        pale: '#F4EAF9',
+      complementary: {
+        red: {
+          evil: 'magenta',
+          dark: '#C82222',
+          regular: '#E13A3A',
+          light: '#F48A8A',
+          pale: '#FFD9D9',
+        },
+        orange: {
+          evil: 'magenta',
+          dark: '#F17A17',
+          regular: '#FF8D2E',
+          light: '#FFC696',
+          pale: '#FFEFE3',
+        },
+        yellow: {
+          evil: 'magenta',
+          dark: '#DAA910',
+          regular: '#ECBE30',
+          light: '#F6DE94',
+          pale: '#FFF4D1',
+        },
+        green: {
+          evil: 'magenta',
+          dark: '#33A797',
+          regular: '#46BFAF',
+          light: '#A2E0D8',
+          pale: '#E7F5F4',
+        },
+        blue: {
+          evil: 'magenta',
+          dark: '#1B84AC',
+          regular: '#2A9CC8',
+          light: '#AADAED',
+          pale: '#E4F4FF',
+        },
+        indigo: {
+          evil: 'magenta',
+          dark: '#121982',
+          regular: '#1A22A2',
+          light: '#9297E5',
+          pale: '#DDE8FD',
+        },
+        purple: {
+          evil: 'magenta',
+          dark: '#5D1D79',
+          regular: '#763294',
+          light: '#B68ACA',
+          pale: '#F4EAF9',
+        },
+        pink: {
+          evil: 'magenta',
+          dark: '#9C2288',
+          regular: '#B930A2',
+          light: '#E39CD7',
+          pale: '#FFE5FB',
+        },
       },
-      pink: {
-        evil: 'magenta',
-        dark: '#9C2288',
-        regular: '#B930A2',
-        light: '#E39CD7',
-        pale: '#FFE5FB',
+      grey: {
+        darkness: '#000000',
+        black: '#4a4a4a',
+        graphite: '#909090',
+        stone: '#B0B0B0',
+        grey: '#C7C7C7',
+        cloudy: '#e4e4e4',
+        pearl: '#f2f2f2',
+        fog: '#fafafa',
+        white: '#ffffff',
+      },
+      status: {
+        info: { evil: 'magenta', dark: '#3499BF', regular: '#4bafd5', light: '#ACD6E6', pale: '#D7E8EE' },
+        success: { evil: 'magenta', dark: '#70A977', regular: '#7dbf85', light: '#BBE1BF', pale: '#DAF1DD' },
+        failure: { evil: 'magenta', dark: '#D12A2A', regular: '#e13a3a', light: '#F3A6A6', pale: '#FFE9E9' },
+        warning: { evil: 'magenta', dark: '#E58D00', regular: '#f59700', light: '#F2C987', pale: '#FDECD2' },
+      },
+      flashMessages: {
+        'grey-dark': '#5b6472',
+        red: '#c74848',
+        orange: '#ff9057',
+        green: '#3cb371',
+        blue: '#2a9cc8',
       },
     },
-    grey: {
-      darkness: '#000000',
-      black: '#4a4a4a',
-      graphite: '#909090',
-      stone: '#B0B0B0',
-      grey: '#C7C7C7',
-      cloudy: '#e4e4e4',
-      pearl: '#f2f2f2',
-      fog: '#fafafa',
-      white: '#ffffff',
-    },
-    status: {
-      info: { evil: 'magenta', dark: '#3499BF', regular: '#4bafd5', light: '#ACD6E6', pale: '#D7E8EE' },
-      success: { evil: 'magenta', dark: '#70A977', regular: '#7dbf85', light: '#BBE1BF', pale: '#DAF1DD' },
-      failure: { evil: 'magenta', dark: '#D12A2A', regular: '#e13a3a', light: '#F3A6A6', pale: '#FFE9E9' },
-      warning: { evil: 'magenta', dark: '#E58D00', regular: '#f59700', light: '#F2C987', pale: '#FDECD2' },
-    },
-    flashMessages: {
-      'grey-dark': '#5b6472',
-      red: '#c74848',
-      orange: '#ff9057',
-      green: '#3cb371',
-      blue: '#2a9cc8',
+  },
+
+  dark: {
+    palette: {
+      primary: {
+        evil: 'magenta',
+        dark: '#1B84AC',
+        regular: '#2A9CC8',
+        light: '#AADAED',
+        pale: '#E4F4FF',
+      },
+      secondary: {
+        evil: 'magenta',
+        dark: '#F17A17',
+        regular: '#FF8D2E',
+        light: '#FFC696',
+        pale: '#FFEFE3',
+      },
+      complementary: {
+        red: {
+          evil: 'magenta',
+          dark: '#C82222',
+          regular: '#E13A3A',
+          light: '#F48A8A',
+          pale: '#FFD9D9',
+        },
+        orange: {
+          evil: 'magenta',
+          dark: '#F17A17',
+          regular: '#FF8D2E',
+          light: '#FFC696',
+          pale: '#FFEFE3',
+        },
+        yellow: {
+          evil: 'magenta',
+          dark: '#DAA910',
+          regular: '#ECBE30',
+          light: '#F6DE94',
+          pale: '#FFF4D1',
+        },
+        green: {
+          evil: 'magenta',
+          dark: '#33A797',
+          regular: '#46BFAF',
+          light: '#A2E0D8',
+          pale: '#E7F5F4',
+        },
+        blue: {
+          evil: 'magenta',
+          dark: '#1B84AC',
+          regular: '#2A9CC8',
+          light: '#AADAED',
+          pale: '#E4F4FF',
+        },
+        indigo: {
+          evil: 'magenta',
+          dark: '#121982',
+          regular: '#1A22A2',
+          light: '#9297E5',
+          pale: '#DDE8FD',
+        },
+        purple: {
+          evil: 'magenta',
+          dark: '#5D1D79',
+          regular: '#763294',
+          light: '#B68ACA',
+          pale: '#F4EAF9',
+        },
+        pink: {
+          evil: 'magenta',
+          dark: '#9C2288',
+          regular: '#B930A2',
+          light: '#E39CD7',
+          pale: '#FFE5FB',
+        },
+      },
+      grey: {
+        darkness: '#000000',
+        black: '#4a4a4a',
+        graphite: '#909090',
+        stone: '#B0B0B0',
+        grey: '#C7C7C7',
+        cloudy: '#e4e4e4',
+        pearl: '#f2f2f2',
+        fog: '#fafafa',
+        white: '#ffffff',
+      },
+      status: {
+        info: { evil: 'magenta', dark: '#3499BF', regular: '#4bafd5', light: '#ACD6E6', pale: '#D7E8EE' },
+        success: { evil: 'magenta', dark: '#70A977', regular: '#7dbf85', light: '#BBE1BF', pale: '#DAF1DD' },
+        failure: { evil: 'magenta', dark: '#D12A2A', regular: '#e13a3a', light: '#F3A6A6', pale: '#FFE9E9' },
+        warning: { evil: 'magenta', dark: '#E58D00', regular: '#f59700', light: '#F2C987', pale: '#FDECD2' },
+      },
+      flashMessages: {
+        'grey-dark': '#5b6472',
+        red: '#c74848',
+        orange: '#ff9057',
+        green: '#3cb371',
+        blue: '#2a9cc8',
+      },
     },
   },
 
@@ -327,26 +478,84 @@ export const defaultTheme: ThemeInitializer = {
   },
 };
 
-//   .d88888b.                                    d8b      888                888                        d8b
-//  d88P" "Y88b                                   Y8P      888                888                        Y8P
-//  888     888                                            888                888
-//  888     888 888  888  .d88b.  888d888 888d888 888  .d88888  .d88b.        888       .d88b.   .d88b.  888  .d8888b
-//  888     888 888  888 d8P  Y8b 888P"   888P"   888 d88" 888 d8P  Y8b       888      d88""88b d88P"88b 888 d88P"
-//  888     888 Y88  88P 88888888 888     888     888 888  888 88888888       888      888  888 888  888 888 888
-//  Y88b. .d88P  Y8bd8P  Y8b.     888     888     888 Y88b 888 Y8b.           888      Y88..88P Y88b 888 888 Y88b.
-//   "Y88888P"    Y88P    "Y8888  888     888     888  "Y88888  "Y8888        88888888  "Y88P"   "Y88888 888  "Y8888P
-//                                                                                                   888
-//                                                                                              Y8b d88P
-//                                                                                               "Y88P"
+//  88888888888 888
+//      888     888
+//      888     888
+//      888     88888b.   .d88b.  88888b.d88b.   .d88b.
+//      888     888 "88b d8P  Y8b 888 "888 "88b d8P  Y8b
+//      888     888  888 88888888 888  888  888 88888888
+//      888     888  888 Y8b.     888  888  888 Y8b.
+//      888     888  888  "Y8888  888  888  888  "Y8888
+//
+//
+//
 
-// Compute once (Singleton)
+export enum Mode {
+  LIGHT = 'light',
+  DARK = 'dark',
+  SYSTEM = 'system',
+}
 
-const { init, ...customThemeRest } = customTheme as Partial<ThemeInitializer>;
-const totalTheme: ITheme = {
-  ...defaultTheme,
-  palette: deepmerge(defaultTheme.palette, customThemeRest.palette || {}),
-}.init();
+const MODE_STORAGE_KEY = 'mode';
+export const isOverrideMultiThemed = 'light' in customTheme && 'dark' in customTheme;
 
-if (init) init.call(totalTheme);
+let displayedTheme = {};
+export default displayedTheme;
 
-export default totalTheme;
+export namespace Theme {
+  export const withAppearanceMode = Component => {
+    return connect((state: any) => ({
+      mode: state.user.mode,
+    }))(Component);
+  };
+
+  const getOverridenTheme = (mode: Mode) => {
+    const { light, dark, ...defaultThemeRest } = defaultTheme;
+    const { init, ...customThemeRest } = customTheme as Partial<ThemeInitializer>;
+    const overridenTheme: ITheme = isOverrideMultiThemed
+      ? {
+          ...defaultThemeRest,
+          palette: deepmerge(defaultTheme[mode].palette, defaultTheme[mode].palette || {}),
+        }.init()
+      : {
+          ...defaultThemeRest,
+          palette: deepmerge(defaultTheme[mode].palette, customThemeRest.palette || {}),
+        }.init();
+    if (init) init.call(overridenTheme);
+    return overridenTheme;
+  };
+
+  export const setMode = (mode?: Mode) => async (dispatch: ThunkDispatch<any, any, any>) => {
+    const selectedMode = (mode === Mode.SYSTEM ? useColorScheme() : mode) || Mode.LIGHT;
+    try {
+      if (mode) await setItemJson(MODE_STORAGE_KEY, mode);
+      displayedTheme = getOverridenTheme(selectedMode);
+      Appearance.setColorScheme(selectedMode);
+      dispatch({ type: actionTypes.setMode, value: mode });
+    } catch {
+      // In case of error, we keep the "light" mode
+      displayedTheme = getOverridenTheme(Mode.LIGHT);
+      Appearance.setColorScheme(Mode.LIGHT);
+      dispatch({ type: actionTypes.setMode, value: Mode.LIGHT });
+    }
+  };
+
+  export const getMode = (state: IGlobalState) => state.user.mode;
+
+  export const init = async () => {
+    let modeSetting;
+    try {
+      modeSetting = (await getItemJson(MODE_STORAGE_KEY)) as Mode | undefined;
+    } catch {
+      // We leave the catch to avoid a crash in case of error
+    } finally {
+      setMode(modeSetting);
+      if (isOverrideMultiThemed) {
+        Appearance.addChangeListener(() => {
+          const selectedMode = getStore().getState().mode;
+          if (selectedMode === Mode.SYSTEM) setMode(Mode.SYSTEM);
+        });
+      }
+    }
+  };
+}
