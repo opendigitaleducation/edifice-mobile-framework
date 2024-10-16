20ff995136d0161d89acd4ac2453ae1fa5eb086d
leadmrn <leadeamorin.ps@gmail.com>
2024-10-16 16:39:15 +0200

    chore(deeplinks): edit native files before apply override

--- ios/appe/AppDelegate.mm
+++ ios/appe/AppDelegate.mm
@@ -141,4 +141,18 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
   } @catch (NSException *exception){}
 }
 
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
+
 @end
