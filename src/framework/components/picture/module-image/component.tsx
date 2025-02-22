d7bd37626ea665e996cbcc63be0873a3c5c6ce21
Valentin Mourot <valentin.mourot@edifice.io>
2025-02-22 16:08:19 +0100

    review: vmou comments

--- src/framework/components/picture/module-image/component.tsx
+++ src/framework/components/picture/module-image/component.tsx
@@ -31,6 +31,11 @@ const ImageFallbackWithModuleConfig: React.FC<ImageFallbackType> = ({ moduleConf
   return <View style={[styles.moduleImage, { backgroundColor: displayColor?.pale }]}>{renderPicture()}</View>;
 };
 
+/** @vmou
+ * La grosse difficulté du composant ci-dessus va être d'appliquer le bon style selon le type de Picture.
+ * Ça va vraiment pas être une mince affaire, et dans un premier temps je te conseille de te focus sur le cas Svg qui est celui qui va nous servir.
+ */
+
 const ImageFallbackWithoutModuleConfig: React.FC<Partial<PictureProps>> = () => {
   return (
     <View style={[styles.moduleImage, { backgroundColor: theme.palette.grey.pearl }]}>
@@ -38,6 +43,11 @@ const ImageFallbackWithoutModuleConfig: React.FC<Partial<PictureProps>> = () => 
     </View>
   );
 };
+
+/** @vmou
+ * Ici tu n'as pas besoin d'avoit deux composants pour le FallbackImage. Un seul suffit,
+ * tu peux juste prévoir une valeur par défaut à moduleConfig dans le cas où il n'est pas disponible (ce qui n'arrivera jamais normalement à part en développement)
+ */
 
 const ImageLoader: React.FC = () => (
   <View style={[styles.moduleImage, styles.imageLoader]}>
@@ -64,5 +74,20 @@ const ModuleImage: React.FC<ModuleImageProps> = ({ moduleConfig, source, ...prop
     </View>
   );
 };
+
+/** @vmou
+ * <ModuleImage/> est en effet un composant qui va contenir une condition en fonction de son état afin d'affichier un sous-composant ou un autre.
+ * Il y a 3 cas possibles :
+ * - chargement
+ * - image affichée
+ * - image non fournie ou échec de chargement
+ *
+ * Tu peux utiliser un simple if/else-if/else pour gérer ces trois cas selon le state. A priori, pas besoin d'avoir plus que ça (donc pas de renderFunction par exmeple).
+ * Attention toutefois )à cette logique car il y a quelque chose qui cloche :
+ * -> C'est le onLoad du composant <Image> qui va permettre de détecter que le chargement a réussi, donc <ImageLoader/> ne te donnera aucun information.
+ * -> Sinon tu seras d'abord obligée de fetch l'image, la stocker, puis la placer dans un composant <Image/> ce qui est contre-productif.
+ * -> Il y a la prop `loadingIndicatorSource` pour afficher quelque chose pendant le chargement.
+ * -> Mais tu peux aussi afficher les deux éléments (<Image> et Placeholder) l'un par-dessus l'autre pendant le chargement pour contourner les limites de `loadingIndicatorSource`.
+ */
 
 export default ModuleImage;
--- src/framework/components/picture/module-image/types.ts
+++ src/framework/components/picture/module-image/types.ts
@@ -3,6 +3,12 @@ import { ImageProps, ImageSourcePropType } from 'react-native';
 import { PictureProps } from '~/framework/components/picture';
 import { AnyNavigableModuleConfig } from '~/framework/util/moduleTool';
 
+/** @vmou
+ * Ici, en soi, tu peux utiliser pick<AnyNavigableModuleConfig, 'displayPicture' | 'displayColor'> pour obtenir juste les deux champs dont tu auras besoin.
+ * Je pense que cette prop ne doit pas être optionelle.
+ * Pour les mêmes raisons, il ne vaut mieux pas utiliser Partial<ImageProps>. Surtout que `source` est déjà optionnel dedans.
+ */
+
 export type ModuleImageProps = Partial<ImageProps> & {
   moduleConfig?: Partial<AnyNavigableModuleConfig>;
   source?: ImageSourcePropType;
@@ -11,6 +17,10 @@ export type ModuleImageProps = Partial<ImageProps> & {
 export type ImageFallbackType = Partial<PictureProps> & {
   moduleConfig: Partial<AnyNavigableModuleConfig>;
 };
+
+/** @vmou
+ * Pas besoin d'utiliser une enum string ici. `const enum` est aussi approprié car on n'a pas besoin de garder la lisibilité de la valeur dans un autre contexte.
+ */
 
 export enum ImageLoadingState {
   LOADING = 'Loading',
